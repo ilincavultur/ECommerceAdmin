4fa1e3442abf8840cc12ba35d2bfe30258e8f5c5
ilincav00 <ilincav00@unet.univie.ac.at>
2023-06-23 19:14:52 +0200

    add and edit category

---
+++ pages/categories.js
@@ -0,0 +1,78 @@
+import Layout from "@/components/Layout";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+export default function Categories() {
+    const [editedCategory, setEditedCategory] = useState(null);
+    const [name, setName] = useState('');
+    const [categories, setCategories] = useState([]);
+    const [parentCategory, setParentCategory] = useState("");
+    useEffect( () => {
+        fetchCategories();
+    }, [])
+    function fetchCategories() {
+        axios.get('/api/categories').then(result => {
+            setCategories(result.data);
+        });
+    }
+    async function saveCategory(ev) {
+        ev.preventDefault();
+        const data = {name, parentCategory};
+        if (editedCategory) {
+            data._id = editedCategory._id;
+            await axios.put('/api/categories', data);
+            setEditedCategory(null);
+        } else {
+            await axios.post('/api/categories', data);
+        }
+
+        setName('');
+        fetchCategories();
+    }
+    function editCategory(category) {
+        setEditedCategory(category);
+        setName(category.name);
+        setParentCategory(category.parent?._id);
+    }
+    return (
+        <Layout>
+            <h1>Categories</h1>
+            <label>
+                {editedCategory ? `Edit Category ${editedCategory.name}` : 'Create New Category'}
+            </label>
+            <form onSubmit={saveCategory} className="flex gap-1">
+                <input className="mb-0" type="text" placeholder={'Category Name'} onChange={ev => setName(ev.target.value)} value={name}/>
+                <select className="mb-0" value={parentCategory} onChange={ev => setParentCategory(ev.target.value)}>
+                    <option value="">No Parent Category</option>
+                    {categories.length > 0 && categories.map(category => (
+                        // eslint-disable-next-line react/jsx-key
+                        <option value={category._id}>{category.name}</option>
+                    ))}
+                </select>
+                <button type={'submit'} className="btn-primary py-1">Save</button>
+            </form>
+            <table className="basic mt-4">
+                <thead>
+                <tr>
+                    <td>Category Name</td>
+                    <td>Parent Category</td>
+                    <td></td>
+                </tr>
+                </thead>
+                <tbody>
+                {categories.length > 0 && categories.map(category => (
+                    // eslint-disable-next-line react/jsx-key
+                    <tr>
+                        <td>{category.name}</td>
+                        <td>{category?.parent?.name}</td>
+                        <td>
+                            <button onClick={() => editCategory(category)} className="btn-primary mr-1">Edit</button>
+                            <button className="btn-primary mr-1">Delete</button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </Layout>
+    )
+}
